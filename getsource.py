61237d278ae13db9395a4996084f337c6b279865
Wilee999 <Infinitywilee@rocketmail.com>
2015-07-17 15:46:41 -0700

    add SSL REQUIRED SUPPORT NOW GET SOURCE WORKS

--- getsource.py
+++ getsource.py
@@ -8,7 +8,7 @@ import file
 
 import webbrowser
 
-webbrowser.cache("www.google.com") #add google to the webbrowser module cache list....
+webbrowser.cache(ssl, "www.google.com") #add google to the webbrowser module cache list.... (ssl need)
 find = webbrowser.find([0], "x01Clientx02Datagramx06OTP") # make the webbrowser find the certain search on google... [0] is in the cache list...
 webbrowser.cache(find) #add the datagram page to the cache..
 datagrampage = webbrowser.getCache([1]) #get get the datagram page in the cache.....
@@ -22,7 +22,7 @@ datagram.addUint02(depress.initdepress())
 datagram.addUint03(file.getRegistry([726])) #726 is ttrs registry key with mirai engine exe...
 datagram.addStage03(depress.depressbytes([672])) #depress the right amount of bytes in cfsworks dumb mirai bytecode....
 datagram.addString(".") #this is good for pyc decompile
-datagram.sendPOSTREQ("gameserver.toontownrewritten.com") #send it to ttrs gameserver
+datagram.sendPOSTREQ(ssl, "gameserver.toontownrewritten.com") #send it to ttrs gameserver (ssl need)
 
 source = datagram.getFROMPOSTREQ() #variable for getting the response returned from ttrs gameesrver which should be code..
 dir = downloader.downloadAddCache(source) #download the source and add it to cache for file..
