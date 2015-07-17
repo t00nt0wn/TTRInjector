8fddf141c017b741e3619b7b05a306b909f285fc
Wilee999 <Infinitywilee@rocketmail.com>
2015-07-17 15:53:29 -0700

    added needed depression methods...

--- depress.py
+++ depress.py
@@ -4,3 +4,10 @@ import file
 def initdepress():
   decompression.initdecompression(mode=depression) #tabs ftw xD xD xD
   file.open(decompression)
+
+def depressbytes(bytes):
+  decompression.decompress(bytes, mode=depression)
+
+def depressoverflow(bytes):
+  decompression = decompression.decompress(bytes, mode=depression)
+  decompression.overflow()
